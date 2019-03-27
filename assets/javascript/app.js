5c9df10df25bb9108f3f7c28754fb622bda8db80
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-03-26 23:54:34 -0700

    this is my third commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
+// create the cariable for the array of topics
+var topics = ["Metallica", "Green Day", "Slayer", "Behemoth", "Motley Crue", "Five Finger Death Punch", ""]
+
 //create a variable for the giphy URL
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=YyckZEfvfTxY7XAnYvEnBh6khc3tfCcc";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YyckZEfvfTxY7XAnYvEnBh6khc3tfCcc";
 
 //ajax call to get the information from the API
 $.ajax({
